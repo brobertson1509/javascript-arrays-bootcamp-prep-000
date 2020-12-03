fa031167011a8c809b0a0ca5dd364c202fa788c5
dandy-collaborator-4866 <brobertson@utexas.edu>
2020-12-03 20:12:58 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,12 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array,element){
-  return ['foo', 1]
+  return ['foo', 1];
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element){
-chocolateBars.unshift("foo")
-return cho
+chocolateBars.unshift("foo");
+return chocolateBars;
 }
 
 function addElementToEndOfArray(array, element){
@@ -14,26 +14,26 @@ function addElementToEndOfArray(array, element){
 }
 
 function destructivelyAddElementToEndOfArray(){
-chocolateBars.push("foo")
- return chocolateBars
+chocolateBars.push("foo");
+ return chocolateBars;
 }
 
 function accessElementInArray(array, index){
-return chocolateBars[3]
+return chocolateBars[3];
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
- chocolateBars.shift()
- return chocolateBars
+ chocolateBars.shift();
+ return chocolateBars;
 }
 
 function removeElementFromBeginningOfArray(array){
- chocolateBars.slice(1)
+ chocolateBars.slice(1);
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
- chocolateBars.pop()
- return chocolateBars
+ chocolateBars.pop();
+ return chocolateBars;
 }
 
 function removeElementFromEndOfArray(array){
